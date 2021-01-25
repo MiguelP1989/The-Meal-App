a75ce1669d3355fcd331846ca2033d8e8d26c90c
MiguelP <j.patrocinio.staiy@gmail.com>
2021-01-25 15:35:56 +0100

    Configured Header with navigationOptions in CategoriesScreen screen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,31 +1,56 @@
 // Third-party imports
 import React from "react";
-import { Text, View, StyleSheet, Button, FlatList } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  FlatList,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
 
 // Global imports
 import { CATEGORIES } from "../data/dummy-data";
+import Colors from "../constants/Colors";
 
 // Local imports
 
 ////////////////////////////////////////////////////////////////////////////////
 
-const renderGridItem = (itemData) => {
-  return (
-    <View>
-      <Text>{itemData.item.title}</Text>
-    </View>
-  );
+const CategoriesScreen = ({ navigation }) => {
+  const renderGridItem = (itemData) => {
+    // Props
+    const touchableOpacityProps = {
+      style: styles.gridItem,
+      onPress: () => navigation.navigate({ routeName: "CategoryMeals" }),
+    };
+    return (
+      <TouchableOpacity {...touchableOpacityProps}>
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
+
+  // Props
+  const flatListProps = {
+    keyExtractor: (item) => item.id,
+    data: CATEGORIES,
+    renderItem: renderGridItem,
+    numColumns: 2,
+  };
+
+  return <FlatList {...flatListProps} />;
 };
 
-const CategoriesScreen = ({}) => {
-  return (
-    <FlatList
-      keyExtractor={(item) => item.id}
-      data={CATEGORIES}
-      renderItem={renderGridItem}
-      numColumns={2}
-    />
-  );
+CategoriesScreen.navigationOptions = {
+  headerTitle: "Meal Categories",
+  headerStyle: {
+    backgroundColor: Platform.OS === "android" ? Colors.primary : "",
+  },
+  headerTintColor: Platform.OS === "android" ? "white" : Colors.primary,
 };
 
 export default CategoriesScreen;
@@ -35,5 +60,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
   },
 });
